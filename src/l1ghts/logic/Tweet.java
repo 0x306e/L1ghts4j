9281f3827e0d9abf6a2c18b7bf5fef83df9cd192
0x306e <3so.d.na@gmail.com>
2017-06-07 02:56:12 +0900

    メソッド情報の追加

--- src/l1ghts/logic/Tweet.java
+++ src/l1ghts/logic/Tweet.java
@@ -6,24 +6,45 @@ import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 
+/**
+ * ツイートに関する処理のクラス
+ * @author 3sodn
+ */
 public class Tweet {
-
+  
   Twitter twitter;
 
   public Tweet() {
     twitter = TwitterFactory.getSingleton();
   }
 
-  public void NormalTweet(String Message) throws TwitterException {
-    twitter.updateStatus(Message);
+  /**
+   * 通常のツイートを送信するメソッド
+   * @param message ツイートするテキスト
+   * @throws TwitterException
+   */
+  public void NormalTweet(String message) throws TwitterException {
+    twitter.updateStatus(message);
   }
 
-  public void ReplyTweet(String Message, long id) throws TwitterException {
-    twitter.updateStatus(new StatusUpdate(Message).inReplyToStatusId(id));
+  /**
+   * 特定のツイートにリプライを送るメソッド
+   * @param message ツイートするテキスト
+   * @param id 送信先のStatus id
+   * @throws TwitterException
+   */
+  public void ReplyTweet(String message, long id) throws TwitterException {
+    twitter.updateStatus(new StatusUpdate(message).inReplyToStatusId(id));
   }
 
-  public void ReplyTweet(String Message, Status status) throws TwitterException {
-    twitter.updateStatus(new StatusUpdate(Message).inReplyToStatusId(status.getId()));
+  /**
+   * 特定のツイートにリプライを送るメソッド
+   * @param message ツイートするテキスト
+   * @param status 送信先のStatus
+   * @throws TwitterException
+   */
+  public void ReplyTweet(String message, Status status) throws TwitterException {
+    twitter.updateStatus(new StatusUpdate(message).inReplyToStatusId(status.getId()));
   }
 
 }
