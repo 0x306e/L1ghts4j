75d71598839d91f51b19efb78559d4c97fde313f
0x306e <3so.d.na@gmail.com>
2017-06-07 13:49:35 +0900

    メソッド情報の追加

--- src/l1ghts/util/Logger.java
+++ src/l1ghts/util/Logger.java
@@ -10,38 +10,59 @@ import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
+/**
+ * @author 3sodn
+ * ログ出力をするクラス
+ */
 public class Logger {
+	
   private SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM");
   private DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH:mm:ss.SSS(xxxxx)");
 
   private Date date = new Date();
   private ZonedDateTime zdt = ZonedDateTime.now();
-
   private File file = new File("log");
+
+  private String OUTPUT_DIRECTORY = "log\\" + sdf.format(date) + "-L1ghts4j.log";
+  private String LOG_HEADER = "[" + dtf.format(zdt) + "]";
 
   public Logger() {
     if (file.exists() == false)
       file.mkdirs();
   }
 
-  public void outputStartLog() throws Exception {
+  /**
+ * 開始時のログを出力するメソッド 
+ * @throws Exception
+ */
+public void outputStartLog() throws Exception {
     PrintWriter pw = new PrintWriter(
-        new BufferedWriter(new FileWriter(("log\\" + sdf.format(date) + "-L1ghts4j.log"), true)));
-    pw.println("[" + dtf.format(zdt) + "][INFO]  L1ghts4j has started.");
+        new BufferedWriter(new FileWriter((OUTPUT_DIRECTORY), true)));
+    pw.println(LOG_HEADER + "[INFO]  L1ghts4j has started.");
     pw.close();
   }
 
-  public void ouputUpdateNameLog(String message) throws IOException {
+  /**
+ * 標準的なログを出力するメソッド
+ * @param message 出力するログメッセージ
+ * @throws IOException
+ */
+public void ouputLog(String message) throws IOException {
     PrintWriter pw = new PrintWriter(
-        new BufferedWriter(new FileWriter(("log\\" + sdf.format(date) + "-L1ghts4j.log"), true)));
-    pw.println("[" + dtf.format(zdt) + "][INFO]  " + message);
+        new BufferedWriter(new FileWriter((OUTPUT_DIRECTORY), true)));
+    pw.println(LOG_HEADER + "[INFO]  " + message);
     pw.close();
   }
 
-  public void ouputErrorLog(String message) throws IOException {
+  /**
+ * エラーログを出力するメソッド
+ * @param message 出力するログメッセージ
+ * @throws IOException
+ */
+public void ouputErrorLog(String message) throws IOException {
     PrintWriter pw = new PrintWriter(
-        new BufferedWriter(new FileWriter(("log\\" + sdf.format(date) + "-L1ghts4j.log"), true)));
-    pw.println("[" + dtf.format(zdt) + "][ERROR] " + message);
+        new BufferedWriter(new FileWriter((OUTPUT_DIRECTORY), true)));
+    pw.println(LOG_HEADER + "[ERROR] " + message);
     pw.close();
   }
 
