b0c9db3de149a5b95299d98ae29c1b8d5894cb14
0x306e <3so.d.na@gmail.com>
2017-06-07 17:03:46 +0900

    メソッド情報の追加、読み込み先をtwitter4jに変更

--- src/l1ghts/account/SettingReader.java
+++ src/l1ghts/account/SettingReader.java
@@ -6,14 +6,24 @@ import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * @author 3sodn
+ * twitter4j.propertiesから情報を読み取るクラス
+ */
 public class SettingReader {
+  
+  private String DEBUG_VALUE = "debug";
+  private String CONSUMER_KEY = "oauth.consumerKey";
+  private String CONSUMER_KEY_SECRET = "oauth.consumerSecret";
+  private String ACCESS_TOKEN = "oauth.accessToken";
+  private String ACCESS_TOKEN_SECRET = "oauth.accessTokenSecret";
 
   private String Reader(String targetName) throws IOException {
     String read = "", key = "";
-    BufferedReader br = new BufferedReader(new FileReader("AccountSetting.txt"));
+    BufferedReader br = new BufferedReader(new FileReader("twitter4j.properties"));
     Pattern formatTarget = Pattern.compile(String.format("%s=(.{1,50})", targetName));
     Matcher matchTarget;
-
+    
     do {
       read = br.readLine();
       System.out.println("got str is " + read);
@@ -22,31 +32,58 @@ public class SettingReader {
       if (matchTarget.find()) {
         key = matchTarget.group(1);
       }
-
     } while (key == "");
 
     br.close();
+    
+    System.out.println(key);
     return key;
 
   }
 
+  /**
+   * @return Debugのtrue/falseを返すメソッド
+   * @throws IOException
+   */
+  public boolean getDebugValue() throws IOException
+  {
+    boolean value = "true".equals(this.Reader(DEBUG_VALUE));
+    return value;
+  }
+  
+  /**
+   * @return　Access Tokenのデータを返すメソッド
+   * @throws IOException
+   */
   public String getAccessToken() throws IOException {
-    String at = this.Reader("AccessToken");
+    String at = this.Reader(ACCESS_TOKEN);
     return at;
   }
 
+  /**
+   * @return Access Token Secretのデータを返すメソッド
+   * @throws IOException
+   */
   public String getAccessTokenSecret() throws IOException {
-    String as = this.Reader("AccessTokenSecret");
+    String as = this.Reader(ACCESS_TOKEN_SECRET);
     return as;
   }
 
+  /**
+   * @return Consumer keyのデータを返すメソッド
+   * @throws IOException
+   */
   public String getConsumerKey() throws IOException {
-    String ck = this.Reader("ConsumerKey");
+    String ck = this.Reader(CONSUMER_KEY);
     return ck;
   }
 
+  /**
+   * @return Consumer Secretのデータを返すメソッド
+   * @throws IOException
+   */
   public String getConsumerSecret() throws IOException {
-    String cs = this.Reader("ConsumerSecret");
+    String cs = this.Reader(CONSUMER_KEY_SECRET);
     return cs;
   }
 
