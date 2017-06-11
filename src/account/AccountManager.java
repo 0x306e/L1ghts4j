983a2565a32bacfee1db0b9336208dc6b12c63a7
0x306e <3so.d.na@gmail.com>
2017-06-11 23:37:24 +0900

    　

--- src/account/AccountManager.java
+++ src/account/AccountManager.java
@@ -17,13 +17,12 @@ public class AccountManager {
    */
   public static Configuration getConfig() throws IOException {
     ConfigurationBuilder builder = new ConfigurationBuilder();
-    SettingReader settingReader = new SettingReader();
   
-    builder.setDebugEnabled(true);
-    builder.setOAuthConsumerKey(settingReader.getConsumerKey());
-    builder.setOAuthConsumerSecret(settingReader.getConsumerSecret());
-    builder.setOAuthAccessToken(settingReader.getAccessToken());
-    builder.setOAuthAccessTokenSecret(settingReader.getAccessTokenSecret());
+    builder.setDebugEnabled(SettingReader.getDebugValue());
+    builder.setOAuthConsumerKey(SettingReader.getConsumerKey());
+    builder.setOAuthConsumerSecret(SettingReader.getConsumerSecret());
+    builder.setOAuthAccessToken(SettingReader.getAccessToken());
+    builder.setOAuthAccessTokenSecret(SettingReader.getAccessTokenSecret());
   
     Configuration conf = builder.build();
     return conf;
