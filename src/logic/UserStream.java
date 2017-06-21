b1287fa3b667e83ecd8e9d4416125631ff274e9a
0x306e <3so.d.na@gmail.com>
2017-06-22 00:26:45 +0900

    なんかスッキリした

--- src/logic/UserStream.java
+++ src/logic/UserStream.java
@@ -13,39 +13,36 @@ import twitter4j.UserStreamAdapter;
 import twitter4j.conf.Configuration;
 
 public class UserStream extends Thread {
+  UpdateName un = new UpdateName();
+  UserStreamAdapter usAdapter;
+
+  public UserStream() {
+    usAdapter = new UserStreamAdapter() {
+      public void onStatus(Status status) {
+        try {
+          un.updateNameCall(status);
+        } catch (TwitterException | IOException e) {
+          e.printStackTrace();
+        }
+      };
+    };
+  }
 
   public void ConnectUserStream() throws IllegalStateException, TwitterException, IOException {
 
     Configuration conf = AccountManager.getConfig();
     Twitter twitter = new TwitterFactory(conf).getInstance();
     TwitterStream twitterStream = new TwitterStreamFactory(conf).getInstance();
-    UpdateName updateName = new UpdateName();
 
-    twitterStream.addListener(new UserStreamAdapter() {
-      public void onStatus(Status status) {
-        try {
-          updateName.updateNameCall(status);
-        } catch (TwitterException e) {
-          e.printStackTrace();
-        } catch (IOException e) {
-          e.printStackTrace();
-        }
-      }
-
-    });
-
+    twitterStream.addListener(usAdapter);
     twitterStream.user();
   }
 
   @Override
   public void run() {
     try {
-      this.ConnectUserStream();
-    } catch (IllegalStateException e) {
-      e.printStackTrace();
-    } catch (TwitterException e) {
-      e.printStackTrace();
-    } catch (IOException e) {
+      ConnectUserStream();
+    } catch (IllegalStateException | TwitterException | IOException e) {
       e.printStackTrace();
     }
   }
