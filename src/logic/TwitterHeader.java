b15e31dc527ce9a5f2b42220333d2d8170105443
0x306e <3so.d.na@gmail.com>
2017-06-25 08:48:04 +0900

    Updater->TwitterHeaderに変更、setTimeHeaderの追加

--- src/logic/TwitterHeader.java
+++ src/logic/TwitterHeader.java
@@ -1,16 +1,20 @@
 package logic;
 
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
 import twitter4j.Twitter;
 import util.Logger;
 
-public class Updater {
+public class TwitterHeader {
   protected Twitter twitter;
   protected Tweet tweet;
   protected Logger logger;
 
   protected final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("(yyyy/MM/dd_HH:mm:ss.SSS)");
-  protected static String TIME_FOOTER;
+  protected static String TIME_HEADER;
 
+  public void setTimeHeader() {
+    TIME_HEADER = dtf.format(ZonedDateTime.now());
+  }
 }
