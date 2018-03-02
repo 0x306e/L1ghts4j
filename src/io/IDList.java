2d8e761f20ab499994722617a84ee9ef5b43e7b9
0x306e <3so.d.na@gmail.com>
2018-03-03 02:17:11 +0900

    static化

--- src/io/IDList.java
+++ src/io/IDList.java
@@ -5,53 +5,51 @@ import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.List;
 
+public final class IDList {
+    private static Path path = FileSystems.getDefault().getPath("IDList.conf");
+    private static List<String> IDs;
 
-public class IDList {
-	private Path path = FileSystems.getDefault().getPath("IDList.conf");
-	private List<String> IDs;
 
-	public IDList() {
-		IDs = new ArrayList<String>();
-	}
-	
-	public void clear() {
-		this.IDs.clear();
-	}
+    public static void clear() {
+        IDs.clear();
+    }
 
-	public IDList load() {
-		this.clear();
-		try {
-			this.IDs = Files.readAllLines(path);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return this;
-	}
+    public static void load() {
+        clear();
+        try {
+            IDs = Files.readAllLines(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public void store() {
-		if (this.IDs.isEmpty()) {
-			return;
-		}
-		try {
-			BufferedWriter bw = Files.newBufferedWriter(path);
-			IDs.forEach(s -> {
-				try {
-					bw.write(s + "\n");
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			});
-			bw.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-	}
-	
-	public List<String> getIDs() {
-		return this.IDs;
-	}
+    public static void store() {
+        if (IDs.isEmpty()) {
+            throw new IllegalStateException("ID is not set.");
+        }
+        try {
+            BufferedWriter bw = Files.newBufferedWriter(path);
+            IDs.forEach(s -> {
+                try {
+                    bw.write(s + "\n");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static List<String> getIDs() {
+        return IDs;
+    }
+
+    public static void add(String id) {
+        IDs.add(id);
+    }
 }
